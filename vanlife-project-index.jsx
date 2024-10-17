30fb65bc34d5af14794abb1c496c70f7494587c3
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-10-17 13:40:33 +0530

    Update vanlife-project-index.jsx

--- vanlife-project-index.jsx
+++ vanlife-project-index.jsx
@@ -17,3 +17,26 @@ import {BrowserRouter,Routes,Route,Link} from "react-router-dom"
  * Review challenge: do all the CSS yourself based on the design
  * linked in the slides.
  */
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
