abc304d152742040cf35bbe8043ba6ab80e5a0b7
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-11-01 19:17:31 +0530

    Update index.jsx

--- add-host-vans-roues/index.jsx
+++ add-host-vans-roues/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
+import Home from "./Home"
+import About from "./About"
 import Vans from "./pages/Vans/Vans"
 import VanDetail from "./pages/Vans/VanDetail"
 import Dashboard from "./pages/Host/Dashboard"
